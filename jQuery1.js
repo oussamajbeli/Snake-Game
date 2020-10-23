4ea7af42ea4d56048fbade0f1a05eb9c172e659a
Oussama Jbeli <60098898+oussamajbeli@users.noreply.github.com>
2020-10-23 19:12:30 +0100

    Delete jQuery1.js

--- jQuery1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-  $("#btn").on("click",function(){
-    alert("vous aver cliqué!");
-  });
-  $("#btn").mouseleave(function(){
-    alert("vous avez relachée le clique !");
-  });
-});
